b25bf691f6ed0a25722fbb275c62e84483727889
Yann GESLIN <ygeslin@e1r8p15.clusters.42paris.fr>
2022-07-07 11:47:22 +0200

    add midd

--- MinAvgTwoSlice.js
+++ MinAvgTwoSlice.js
@@ -37,3 +37,62 @@
 
 // N is an integer within the range [2..100,000];
 // each element of array A is an integer within the range [−10,000..10,000].
+
+// ! my function
+function solution(A) {
+    // write your code in JavaScript (Node.js 8.9.4)
+    let avg = (A[0] + A[1]) / 2;
+    let ret = 0;
+    let min = avg;
+    for (let i = 0; i < A.length - 1;i++)
+    {
+        avg = (A[i] + A[i + 1]) / 2;
+        if (avg < min)
+        {
+            min = avg;
+            ret = i;
+        }
+    }
+    for (let i = 0; i < A.length - 2;i++)
+    {
+        avg = (A[i] + A[i + 1] + A[i + 2]) / 3;
+        if (avg < min)
+        {
+            min = avg;
+            ret = i;
+        }
+    }
+    return ret;
+}
+
+function solution ( A )
+{
+	// write your code in JavaScript (Node.js 8.9.4)
+	let B = []
+	let C = []
+	let min = 10000
+	let result = 0;
+	for ( let i = 0; i < A.length - 1; i++ )
+	{
+		B[ i ] = ( A[ i ] + A[ i + 1 ] ) / 2
+		if ( ( B[ i ] ) < min )
+		{
+			min = B[ i ]
+			result = i
+		}
+	}
+
+	let = 0
+	for ( let i = 0; i < A.length - 2; i++ )
+	{
+		C[ i ] = ( A[ i ] + A[ i + 1 ] + A[ i + 2 ] ) / 3
+		if ( ( C[ i ] ) < min )
+		{
+			min = C[ i ]
+			result = i
+		}
+	}
+
+
+	return ( result )
+}
