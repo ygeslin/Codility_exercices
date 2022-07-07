e8856acc2446f1fdfbda3629097b83215e819781
Yann GESLIN <ygeslin@e1r8p15.clusters.42paris.fr>
2022-07-07 15:37:52 +0200

    add soruces for number of distinct intersections

--- MinAvgTwoSlice.js
+++ MinAvgTwoSlice.js
@@ -65,34 +65,34 @@ function solution(A) {
     return ret;
 }
 
-function solution ( A )
-{
-	// write your code in JavaScript (Node.js 8.9.4)
-	let B = []
-	let C = []
-	let min = 10000
-	let result = 0;
-	for ( let i = 0; i < A.length - 1; i++ )
-	{
-		B[ i ] = ( A[ i ] + A[ i + 1 ] ) / 2
-		if ( ( B[ i ] ) < min )
-		{
-			min = B[ i ]
-			result = i
-		}
-	}
+// function solution ( A )
+// {
+// 	// write your code in JavaScript (Node.js 8.9.4)
+// 	let B = []
+// 	let C = []
+// 	let min = 10000
+// 	let result = 0;
+// 	for ( let i = 0; i < A.length - 1; i++ )
+// 	{
+// 		B[ i ] = ( A[ i ] + A[ i + 1 ] ) / 2
+// 		if ( ( B[ i ] ) < min )
+// 		{
+// 			min = B[ i ]
+// 			result = i
+// 		}
+// 	}
 
-	let = 0
-	for ( let i = 0; i < A.length - 2; i++ )
-	{
-		C[ i ] = ( A[ i ] + A[ i + 1 ] + A[ i + 2 ] ) / 3
-		if ( ( C[ i ] ) < min )
-		{
-			min = C[ i ]
-			result = i
-		}
-	}
+// 	let = 0
+// 	for ( let i = 0; i < A.length - 2; i++ )
+// 	{
+// 		C[ i ] = ( A[ i ] + A[ i + 1 ] + A[ i + 2 ] ) / 3
+// 		if ( ( C[ i ] ) < min )
+// 		{
+// 			min = C[ i ]
+// 			result = i
+// 		}
+// 	}
 
 
-	return ( result )
-}
+// 	return ( result )
+// }
